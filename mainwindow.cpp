1109e496df843277cb31ee4a9172e76c31a35932
tiagoGottardo <tiago.panizio@hotmail.com>
2024-10-18 11:31:20 -0300

    first functional version

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,48 +3,68 @@
 
 #include <QPainter>
 
+#include <memory>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 #include <limits>
 
 class Point {
 public:
-    uint32_t x;
-    uint32_t y;
+    int32_t x;
+    int32_t y;
+    int32_t z;
 
-    Point(double x, double y) : x(x), y(y){};
+    std::string name = "Dot";
+
+    Point(int32_t x, int32_t y, int32_t z) : x(x), y(y), z(z) {};
+
+    std::string self(){
+        std::stringstream ss;
+        ss << name << "(" << x << ", " << y << ", " << z << ")";
+        return ss.str();
+    }
 
     void checkItself(){
-        printf("(%d, %d)\n", x, y);
+        std::cout << self() << std::endl;
     };
 
     bool operator==(const Point& other) const {
-        return (x == other.x && y == other.y);
+        return (x == other.x && y == other.y && z == other.z);
     }
 
     bool operator!=(const Point& other) const {
-        return !(*this == other);  // Use the == operator to define !=
+        return !(*this == other);
     }
 };
 
 class Line {
 public:
-    Point a;
-    Point b;
+    Point* a;
+    Point* b;
 
-    Line(Point a, Point b) : a(a), b(b){};
+    std::string name = "Line";
+
+    Line(Point* a, Point* b) : a(a), b(b) {};
+
+    std::string self() {
+        std::stringstream ss;
+        ss << name << "|" << a->self() << " -> " << b->self() << "|";
+        return ss.str();
+    };
 
     void checkItself(){
-        printf("From: (%d, %d)  ", a.x, a.y);
-        printf("To: (%d, %d)\n", b.x, b.y);
+        std::cout << self() << std::endl;
     };
 
     void draw(QPainter* painter){
-        painter->drawLine(a.x, a.y, b.x, b.y);
+        painter->drawLine(a->x, a->y, b->x, b->y);
     }
 
     double calculateAngular(){
-        int dy = b.y - a.y;
-        int dx = b.x - a.x;
+        int32_t dy = b->y - a->y;
+        int32_t dx = b->x - a->x;
 
         if(dx == 0) {
             return std::numeric_limits<double>::infinity();
@@ -56,35 +76,36 @@ public:
 
 class Polygon {
 public:
-    std::vector<Line> lines;
-    Point ref = Point(0, 0);
+    std::vector<Line>* lines;
+    Point ref = Point(0, 0, 0);
 
-    Polygon(std::vector<Line> linesList) {
-        if(linesList.size() <= 2) {
+    std::string name = "Polygon";
+
+    Polygon(std::vector<Line>* linesList) {
+        if(linesList->size() <= 2) {
             printf("You need 3 dots or more to create a polygon.\n");
             return;
         }
 
         bool couldBePolygon = true;
-        for(uint i = 0; i < linesList.size(); i++){
-            if(i + 1 == linesList.size()){
-                if(linesList[i].b != linesList[0].a){
+        for(uint i = 0; i < linesList->size(); i++){
+            if(i + 1 == linesList->size()){
+                if((*linesList)[i].b != (*linesList)[0].a){
                     couldBePolygon = false;
                 }
-                if(linesList[i].calculateAngular() == linesList[i + 1].calculateAngular()){
+                if((*linesList)[i].calculateAngular() == (*linesList)[i + 1].calculateAngular()){
                     printf("There are two lines in place of one.\n");
                     couldBePolygon = false;
                 }
                 break;
-
             }
 
-            if(linesList[i].b != linesList[i + 1].a){
+            if((*linesList)[i].b != (*linesList)[i + 1].a){
                 couldBePolygon = false;
                 break;
             }
 
-            if(linesList[i].calculateAngular() == linesList[i + 1].calculateAngular()){
+            if((*linesList)[i].calculateAngular() == (*linesList)[i + 1].calculateAngular()){
                 printf("There are two lines in place of one.\n");
                 break;
             }
@@ -99,68 +120,55 @@ public:
     };
 
     void checkItself() {
-        for(uint i = 0; i < lines.size(); i++){
-            lines[i].checkItself();
+        printf("Polygon {\n");
+        for(uint i = 0; i < lines->size(); i++){
+            (*lines)[i].checkItself();
         }
+        printf("}\n");
     };
 
     Point calculateCentroid(){
         int Cx = 0, Cy = 0;
-        std::vector<Point> *dots = getDots();
+        std::vector<Point*> dots = getDots();
 
-        uint size = dots->size();
+        uint size = dots.size();
 
         if (size == 0){
             throw std::invalid_argument("There is no dots in centroid calculation.");
         };
 
         for(uint i = 0; i < size; i++){
-            Cx += (*dots)[i].x;
-            Cy += (*dots)[i].y;
+            Cx += dots[i]->x;
+            Cy += dots[i]->y;
         }
 
-        return Point(Cx/size, Cy/size);
+        return Point(Cx/size, Cy/size, 0);
     };
 
-    std::vector<Point>* getDots(){
-        std::vector<Point>*dots = new std::vector<Point>;
+    std::vector<Point*> getDots(){
+        std::vector<Point*> dots;
 
-        for(uint i = 0; i < lines.size(); i++){
-            dots->push_back(lines[i].a);
+        for(uint i = 0; i < (*lines).size(); i++){
+            dots.push_back((*lines)[i].a);
         }
-        dots->push_back(lines.back().b);
 
         return dots;
     };
 
-    void redimensionX(float s){
-        std::vector<Point>* dots = getDots();
+    void redimensionXY(float s){
+        std::vector<Point*> dots = getDots();
 
         Point centroid = calculateCentroid();
 
-        for(uint i = 0; i < dots->size(); i++){
-            (*dots)[i].x = centroid.x + s * ((*dots)[i].x - centroid.x);
+        for(uint i = 0; i < dots.size(); i++){
+            dots[i]->y = centroid.y + s * (dots[i]->y - centroid.y);
+            dots[i]->x = centroid.x + s * (dots[i]->x - centroid.x);
         }
     };
 
-    //void redimensionY(float s){
-    //    std::vector<Point> dots = getDots();
-
-    //    Point centroid = calculateCentroid();
-
-    //    for(uint i = 0; i < dots.size(); i++){
-    //        dots[i].y = centroid.y + s * (dots[i].y - centroid.y);
-    //    }
-    //};
-
-    //void redimensionXY(float s){
-    //    redimensionX(s);
-    //    redimensionY(s);
-    //};
-
     void draw(QPainter* painter){
-        for(uint i = 0; i < lines.size(); i++) {
-            lines[i].draw(painter);
+        for(uint i = 0; i < (*lines).size(); i++) {
+            (*lines)[i].draw(painter);
         }
     }
 };
@@ -172,21 +180,23 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     std::vector<Point> points;
-    std::vector<Line> lines;
+    lines = new std::vector<Line>();
 
-    Point a(50, 50);
-    Point b(100, 200);
-    Point c(300, 300);
+    Point* a = new Point(50, 50, 0);
+    Point* b = new Point(100, 200, 0);
+    Point* c = new Point(300, 300, 0);
 
     Line A(a, b);
     Line B(b, c);
     Line C(c, a);
 
-    lines.push_back(A);
-    lines.push_back(B);
-    lines.push_back(C);
+    lines->push_back(A);
+    lines->push_back(B);
+    lines->push_back(C);
 
     myPolygon = new Polygon(lines);
+
+    myPolygon->checkItself();
 }
 
 MainWindow::~MainWindow()
@@ -203,10 +213,18 @@ void MainWindow::paintEvent(QPaintEvent *event)
     pen.setWidth(1);
     painter.setPen(pen);
 
+
     myPolygon->draw(&painter);
 }
 
 void MainWindow::on_pBx1_clicked() {
-    myPolygon->redimensionX(1.5);
+    myPolygon->redimensionXY(1.5);
     myPolygon->checkItself();
+    update();
+}
+
+void MainWindow::on_pBx2_clicked() {
+    myPolygon->redimensionXY(0.6666666666);
+    myPolygon->checkItself();
+    update();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,12 +3,15 @@
 
 #include <QMainWindow>
 
+#include <vector>
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
 
+class Line;
 class Polygon;
 
 class MainWindow : public QMainWindow
@@ -22,10 +25,12 @@ private:
     Ui::MainWindow *ui;
 
     Polygon* myPolygon;
+    std::vector<Line>* lines;
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 private slots:
     void on_pBx1_clicked();
+    void on_pBx2_clicked();
 };
 #endif // MAINWINDOW_H
