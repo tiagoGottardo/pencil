c13592be2d59f97be7467fff7a0fb5604865a82f
tiagoGottardo <tiago.panizio@hotmail.com>
2024-12-10 16:04:53 -0300

    add point tests

--- tests/Point.cpp
+++ tests/Point.cpp
@@ -12,5 +12,27 @@ void point() {
     return p == Point(20, 10, 237);
   }(), "it tests translation multiplication on point");
 
+  suite.add([]() -> bool { 
+    Matrix m({ {3.43, 0}, {2.9, 0}, {0.1, 0} });
+    Point p = Point();
+
+    p = m;
+
+    return p == Point(3, 3, 0);
+  }(), "it tests matrix to point conversion");
+
+  suite.add([]() -> bool { 
+    Matrix m = Matrix({ {3.43, 0}, {2.9, 0} });
+    Point p = Point();
+
+    try {
+      p = m;
+    } catch(std::exception e) {
+      return true;
+    }
+
+    return false;
+  }(), "it tests invalid matrix to point conversion");
+
   suite.run();
 }
