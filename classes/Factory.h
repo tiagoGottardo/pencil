e86220db5b9bf54066045cc5c0ba49324c13169e
tiagoGottardo <tiago.panizio@hotmail.com>
2024-12-20 11:20:34 -0300

    improve import method performance

--- classes/Factory.h
+++ classes/Factory.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <fstream>
-#include <iostream>
+#include <iostream> 
 #include <sstream>
 
 #include "./Point.h"
@@ -87,51 +87,51 @@ public:
     return Model(polygons, Point(), "Donut");
   };
 
-  static Model import(string filename) {
-    vector<Point> points = vector<Point>();
-    vector<Polygon> polygons = vector<Polygon>();
+  static Model import(const std::string& filename) {
+    vector<Point> points;
+    vector<Polygon> polygons;
+    points.emplace_back(Point());
 
-    points.push_back(Point());
+    ifstream file(filename, std::ios::ate);
+    streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
 
-    ifstream file(filename);
+    vector<char> buffer(size);
+    file.read(buffer.data(), size);
+    istringstream fileContent(buffer.data());
 
     string line;
-
-    while(getline (file, line)) {
+    while (std::getline(fileContent, line)) {
       istringstream lineStream(line);
       string prefix;
       lineStream >> prefix;
 
       if (prefix == "v") {
         double x, y, z;
-
         lineStream >> x >> y >> z;
-        points.push_back(Point(round(x * 1000), round(y * 1000), round(z * 1000)));
-      } 
-
-      if (prefix == "f") {
-        vector<Point> polygonPoints = vector<Point>();
+        points.emplace_back(round(x * 1000), round(y * 1000), round(z * 1000));
+      } else if (prefix == "f") {
+        vector<Point> polygonPoints;
         string slice;
-
-        while(lineStream >> slice) {
-          istringstream sliceStream(slice);
-          string sliceIndex;
-
-          if(std::getline(sliceStream, sliceIndex, '/')) 
-            polygonPoints.push_back(points[stoi(sliceIndex)]);
+        while (lineStream >> slice) {
+          size_t slashPos = slice.find('/');
+          int index = std::stoi(slice.substr(0, slashPos));
+          polygonPoints.push_back(points[index]);
         }
-
-        polygons.push_back(Polygon(polygonPoints));
-      } 
+        polygons.emplace_back(std::move(polygonPoints));
+      }
     }
 
-    size_t lastSlashPos = filename.find_last_of('/');
-    string name = filename.substr(lastSlashPos + 1);
-    size_t dotPos = name.find_last_of('.');
+    string_view nameView(filename);
+    size_t lastSlashPos = nameView.find_last_of('/');
+    if (lastSlashPos != std::string_view::npos) nameView = nameView.substr(lastSlashPos + 1);
 
-    if (dotPos != string::npos) name = name.substr(0, dotPos); 
-    if (!name.empty()) name[0] = toupper(name[0]); 
+    size_t dotPos = nameView.find_last_of('.');
+    if (dotPos != std::string_view::npos) nameView = nameView.substr(0, dotPos);
 
-    return Model(polygons, Point(), name);
+    std::string name(nameView);
+    if (!name.empty()) name[0] = toupper(name[0]);
+
+    return Model(std::move(polygons), Point(), name);
   }
 };
